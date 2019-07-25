df722e46313a1e1e3f056a9aff6d13376ad63be1
Ian Cook Westgate <iancookwestgate@gmail.com>
2019-07-25 15:51:33 -0700

    style profile picture into correct position.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,6 @@ class Home extends React.Component {
     return (
       <div id="home">
         <div id="top-section">
-          <h1>Home component!</h1>
           <div class="image-wrap">
             <img src={profile} alt="A picture of Ian Cook Westgate"></img>
           </div>
