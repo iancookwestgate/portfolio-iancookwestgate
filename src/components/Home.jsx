94980fed41f32a000cfa9c9f1b7652cbb7aad0e7
Ian Cook Westgate <iancookwestgate@gmail.com>
2019-08-02 14:10:13 -0700

    blur home image and flesh out about me section.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../scss/styles.scss';
+import aboutPic from '../assets/images/Ian-Pic.jpg';
 import profile from '../assets/images/Profile-Picture.jpg';
 
 class Home extends React.Component {
@@ -30,6 +31,13 @@ class Home extends React.Component {
         </div>
         <div id="about-me">
           <h1>About Me</h1>
+          <div id="about-content">
+            <div id="about-text-section">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <button>View CV</button>
+            </div>
+            <img src={aboutPic}></img>
+          </div>
         </div>
       </div>
     );
